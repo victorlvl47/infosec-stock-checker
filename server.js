d3b74ec46282523cfd53f345c96a0f24e00f6d6f
victor <victor.lvl47@gmail.com>
2024-05-02 23:35:22 -0600

    only allow loading of scripts and CSS of our server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 require('dotenv').config();
 const express     = require('express');
+const helmet = require('helmet');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
 
@@ -9,6 +10,14 @@ const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
 const app = express();
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"],
+  }
+}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
